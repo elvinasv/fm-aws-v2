190e618823facd8c4935a7f3cf51f98b5def960d
Elvinas V <elvinas.val@gmail.com>
2024-01-12 11:12:30 +0200

    feat: update git action workflows

--- src/Application.js
+++ src/Application.js
@@ -6,12 +6,12 @@ import { Page } from './Page';
 function Application() {
   return (
     <main className="h-screen">
-      <h1 className="text-3xl font-bold bg-yellow-300 p-4 border-b-2 border-yellow-400">
+      <h1 className="text-3xl font-bold bg-indigo-300 p-4 border-b-2 border-indigo-400">
         Incredibly Important Thoughts
       </h1>
       <div className="grid grid-cols-12 grid-rows-1 gap-4 h-full">
         <Navigation>
-          <NavigationLink to="/">Home</NavigationLink>
+          <NavigationLink to="/">Home of the Year</NavigationLink>
           <NavigationLink to="/notes/hipster">Hipster Ipsum</NavigationLink>
           <NavigationLink to="/notes/zombie">Zombie Ipsum</NavigationLink>
           <NavigationLink to="/notes/cupcake">Cupcake Ipsum</NavigationLink>
